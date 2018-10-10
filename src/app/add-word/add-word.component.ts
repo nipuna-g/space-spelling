0522d3863fd9064234a4db9af2337f4c6ef71882
Nipuna Gunathilake <nipuna777@gmail.com>
2018-10-11 00:21:04 +0530

    add word call fixes

--- src/app/add-word/add-word.component.ts
+++ src/app/add-word/add-word.component.ts
@@ -1,41 +1,73 @@
-import { SpellingStoreService } from './../spelling-store.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
-import { WordDefinitionService, WordDefinition } from '../word-definition-service.service';
+import { WordDefinition, WordDefinitionService } from '../word-definition-service.service';
+import { SpellingStoreService } from './../spelling-store.service';
 
 @Component({
-  selector: 'app-add-word',
-  templateUrl: './add-word.component.html',
-  styleUrls: ['./add-word.component.scss']
+    selector: 'app-add-word',
+    templateUrl: './add-word.component.html',
+    styleUrls: ['./add-word.component.scss']
 })
 export class AddWordComponent implements OnInit {
+    public definitionLoading = false;
+    public wordSuggestion: WordDefinition = null;
 
-  public addWordForm = new FormGroup({
-    word: new FormControl('', Validators.required),
-    pronounciaton: new FormControl('', Validators.required),
-    definition: new FormControl(''),
-  })
-
-  constructor(private definitionService: WordDefinitionService, private spellingStoreService: SpellingStoreService) { }
-
-  ngOnInit() {
-    this.addWordForm.get('word').valueChanges.pipe(
-      debounceTime(200)
-    ).subscribe((value) => {
-      if (!value || value.length < 2) return;
-      this.updateWordDetails(value);
+    public addWordForm = new FormGroup({
+        word: new FormControl('', Validators.required),
+        pronounciaton: new FormControl('', Validators.required),
+        definition: new FormControl('')
     });
-  }
 
-  private updateWordDetails(word) {
-    this.definitionService.getWordDetails(word, (definition: WordDefinition) => {
-      this.addWordForm.get('pronounciaton').setValue(definition.phonetic[0]);
-      this.addWordForm.get('definition').setValue(definition.meaning.noun[0].definition);
-    });
-  }
+    constructor(
+        private definitionService: WordDefinitionService,
+        private spellingStoreService: SpellingStoreService
+    ) {}
 
-  public addWord() {
-    this.spellingStoreService.saveWord(this.addWordForm.value)
-  }
+    ngOnInit() {
+        this.addWordForm
+            .get('word')
+            .valueChanges.pipe(debounceTime(200))
+            .subscribe(value => {
+                if (!value || value.length < 2) return;
+                this.updateWordDetails(value);
+            });
+    }
+
+    private updateWordDetails(word) {
+        this.wordSuggestion = null;
+        this.definitionLoading = true;
+        this.definitionService.getWordDetails(
+            word,
+            (definition: WordDefinition) => {
+                if (this.addWordForm.get('word').value === definition.word) {
+                    this.populateWordDefinition(definition);
+                } else {
+                    this.wordSuggestion = definition;
+                }
+
+                this.definitionLoading = false;
+            },
+            error => {
+                console.log(error);
+                this.definitionLoading = false;
+            }
+        );
+    }
+
+    private populateWordDefinition(definition: WordDefinition) {
+        this.addWordForm.get('pronounciaton').setValue(definition.phonetic[0]);
+        this.addWordForm
+            .get('definition')
+            .setValue(definition.meaning.noun[0].definition);
+    }
+
+    public useSuggestion() {
+        this.addWordForm.get('word').setValue(this.wordSuggestion.word);
+        this.populateWordDefinition(this.wordSuggestion);
+    }
+
+    public saveWord() {
+        this.spellingStoreService.saveWord(this.addWordForm.value);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,45 @@
-import { AddWordComponent } from './add-word/add-word.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatProgressBarModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatFormFieldControl, MatInputModule } from '@angular/material';
+import { AddWordComponent } from './add-word/add-word.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpellingStoreService } from './spelling-store.service';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { SpellingStoreService } from './spelling-store.service';
 import { StartCanvasComponent } from './start-canvas/start-canvas.component';
-import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddWordComponent,
-    MainNavComponent,
-    HomeComponent,
-    StartCanvasComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatButtonModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    ScrollDispatchModule,
-  ],
-  providers: [
-    SpellingStoreService,
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AddWordComponent,
+        MainNavComponent,
+        HomeComponent,
+        StartCanvasComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatButtonModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        ScrollDispatchModule,
+        MatProgressBarModule
+    ],
+    providers: [SpellingStoreService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
