101fee1916c9b5346c512470d5f349ad51b13f39
Nipuna Gunathilake <nipuna@ridebeam.com>
2020-05-04 02:25:49 +0800

    wrapping navigator in drawer

--- App.tsx
+++ App.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import 'react-native-gesture-handler';
 import Home from '@pages/Home';
 
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleStyle: {fontFamily: 'Racing Sans One', fontSize: 24},
-        }}>
-        <Stack.Screen name="Spaced Spelling" component={Home} />
-      </Stack.Navigator>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        hideStatusBar={true}
+        drawerContent={(props) => (
+          <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+          </DrawerContentScrollView>
+        )}>
+        <Drawer.Screen name="Home" component={HomeNavigation} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
+
+const HomeNavigation = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerTitleStyle: {fontFamily: 'Racing Sans One', fontSize: 24},
+    }}>
+    <Stack.Screen name="Spaced Spelling" component={Home} />
+  </Stack.Navigator>
+);
 
 export default App;
